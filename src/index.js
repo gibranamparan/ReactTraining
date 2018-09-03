b10a5075cf74dcf3c8e902f9538f631ee95fce79
gibranamparan <gibranamparand@outlook.com>
2018-09-02 22:32:12 -0500

    Created new files

--- src/index.js
+++ src/index.js
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar'
+import VideoListItem from './components/video_list_item'
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = 'AIzaSyAWOeFRwVao_KPTOyNKkOJSJirPJnFi2ag';
+import YTSearch from 'youtube-api-search'
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Create a component
+class App extends Component {
+    constructor(params){
+        super(params);
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: 'pewdiepie'}, (videos) => {
+            this.setState({ videos });
+            console.log(videos);
+        });
+    }
+    
+    render(){
+        return (
+            <div>
+                <div>
+                    <SearchBar/>
+                </div>
+                <div>
+                    <VideoListItem/>
+                </div>
+            </div>
+        );
+    }
+}
+
+// Put it in the page
+ReactDOM.render(<App/>, document.querySelector(".container"));
